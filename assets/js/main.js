498d362a565bf15d9d98d1d8e52a1327349e23b7
Yuval Gelles <ygelles@gmail.com>
2024-08-11 14:54:32 +0300

    Add floating navigation bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function($){
+    // ************ Change Here *************
     window.owner_name = "Firstname M. Surename";
+    var enable_floating_navbar = false;
+    // **************************************
+    
+    window.onscroll = function() {navbarFunction()};
 
     var path = window.location.pathname;
     var page = path.split("/").pop();
@@ -23,6 +28,7 @@ jQuery(document).ready(function($){
         $.get("../elements/nav-smaller.html", function(data){
             $("#nav-placeholder").replaceWith(data);
             document.getElementById('navbar-brand').innerHTML = window.owner_name;
+            enable_floating_navbar = false; // Do not use floating navigation bar in the smaller one
         });
 
         //  Footer
@@ -32,5 +38,15 @@ jQuery(document).ready(function($){
             var year = new Date().getFullYear();
             document.getElementById('footer-copyright').innerHTML = "Copyright @ "+year;
             });
+    }
+
+    function navbarFunction() {
+        if (enable_floating_navbar){
+            if (window.scrollY > 0) {
+                document.getElementById("navbar").classList.add("sticky")
+            } else {
+                document.getElementById("navbar").classList.remove("sticky");
+            }
+        }
     }
 });
